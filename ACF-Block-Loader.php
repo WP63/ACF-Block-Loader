fc21dd18ba699fe84099bb0be09cc1e02816883f
Jirayu Yingthawornsuk <me@jirayu.in.th>
2019-06-20 00:10:16 +0700

    check function exists

--- ACF-Block-Loader.php
+++ ACF-Block-Loader.php
@@ -10,27 +10,29 @@ License: GPLv2 or later
 Text Domain: acfbl
 */
 
-add_action('acf/init', function() {
-  $namespace = apply_filters('wp63/acf_block_namespace', 'WP63\Blocks\\');
-  $directory = get_template_directory() . '/' . apply_filters('wp63/acf_block_directory', 'Blocks');
+if( function_exists('add_action') ) {
+  add_action('acf/init', function() {
+    $namespace = apply_filters('wp63/acf_block_namespace', 'WP63\Blocks\\');
+    $directory = get_template_directory() . '/' . apply_filters('wp63/acf_block_directory', 'Blocks');
 
-  if( !is_dir( $directory ) ) {
-    return;
-  }
+    if( !is_dir( $directory ) ) {
+      return;
+    }
 
-  if( $handle = opendir( $directory ) ) {
-    while( false !== ( $filename = readdir( $handle ) )) {
-      if( $filename === '.' || $filename === '..' ) {
-        continue;
-      }
+    if( $handle = opendir( $directory ) ) {
+      while( false !== ( $filename = readdir( $handle ) )) {
+        if( $filename === '.' || $filename === '..' ) {
+          continue;
+        }
 
-      $parts = pathinfo( $filename );
+        $parts = pathinfo( $filename );
 
-      if( class_exists( $namespace . $parts['filename'] ) ) {
-        $classname = $namespace . $parts['filename'];
+        if( class_exists( $namespace . $parts['filename'] ) ) {
+          $classname = $namespace . $parts['filename'];
 
-        call_user_func( new $classname );
+          call_user_func( new $classname );
+        }
       }
     }
-  }
-});
+  });
+}
