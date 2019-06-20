0186bf3192dbf7308a51eebf05923dbee7f981b8
Jirayu Yingthawornsuk <me@jirayu.in.th>
2019-06-20 20:29:28 +0700

    PrepareRender method

--- src/Block.php
+++ src/Block.php
@@ -2,6 +2,11 @@
 namespace WP63;
 
 abstract class Block {
+  protected $block;
+  protected $post;
+  protected $is_preview;
+  protected $post_id;
+
   /**
    * Abstract method for block initialization
    * @return  array     method MUST return an array contains key `name` as block unique name, and `title` as block actual name
@@ -35,8 +40,21 @@ abstract class Block {
         'name'              => $settings['name'],
         'title'             => $settings['title'],
         'category'          => $settings['category'],
-        'render_callback'   => [$this, 'render'],
+        'render_callback'   => [$this, 'PrepareRender'],
       ]);
     }
+  }
+
+  /**
+   * PrepareRender method.
+   * Run before actual rendering method. Use for manipulating all repetitive data from ACF.
+   */
+  public function PrepareRender( $block, $content = '', $is_preview = false, $post_id = 0 ) {
+    $this->$block = $block;
+    $this->$post = $post;
+    $this->$is_preview = $is_preview;
+    $this->$post_id = $post_id;
+
+    $this->render();
   }
 }
