2104cb5229bd8739254af8540ed94a988152d204
Jirayu Yingthawornsuk <me@jirayu.in.th>
2019-10-02 19:32:02 +0700

    use procedural for init

--- ACF-Block-Loader.php
+++ ACF-Block-Loader.php
@@ -1,49 +1,40 @@
 <?php
 namespace WP63;
 
-class BlockLoader {
-  public static function Init() {
-    add_action( 'admin_notices', [$this, 'VersionNotice'] );
-    add_action( 'acf/init', [$this, 'Loader'] );
+add_action( 'admin_notices', function () {
+  $version = get_option('acf_version');
+  $required_version = '5.8.0';
+  $compare = version_compare( $version, $required_version );
+
+  if( !class_exists('ACF') || $compare === -1 ) {
+    echo "
+    <div class=\"notice notice-warning\">
+      <p><code>wp63/acf-block-loader</code> requires Advanced Custom Field PRO {$required_version} or newer.</p>
+    </div>";
+  }
+} );
+
+add_action( 'acf/init', function () {
+  $namespace = apply_filters('wp63/acf_block_namespace', 'App\Blocks\\');
+  $directory = get_template_directory() . '/' . apply_filters('wp63/acf_block_directory', 'Blocks');
+
+  if( !is_dir( $directory ) ) {
+    return;
   }
 
-  private function VersionNotice() {
-    $version = get_option('acf_version');
-    $required_version = '5.8.0';
-    $compare = version_compare( $version, $required_version );
+  if( $handle = opendir( $directory ) ) {
+    while( false !== ( $filename = readdir( $handle ) )) {
+      if( $filename === '.' || $filename === '..' ) {
+        continue;
+      }
 
-    if( !class_exists('ACF') || $compare === -1 ) {
-      echo "
-      <div class=\"notice notice-warning\">
-        <p><code>wp63/acf-block-loader</code> requires Advanced Custom Field PRO {$required_version} or newer.</p>
-      </div>";
-    }
-  }
+      $parts = pathinfo( $filename );
 
-  private function Loader() {
-    $namespace = apply_filters('wp63/acf_block_namespace', 'App\Blocks\\');
-    $directory = get_template_directory() . '/' . apply_filters('wp63/acf_block_directory', 'Blocks');
+      if( class_exists( $namespace . $parts['filename'] ) ) {
+        $classname = $namespace . $parts['filename'];
 
-    if( !is_dir( $directory ) ) {
-      return;
-    }
-
-    if( $handle = opendir( $directory ) ) {
-      while( false !== ( $filename = readdir( $handle ) )) {
-        if( $filename === '.' || $filename === '..' ) {
-          continue;
-        }
-
-        $parts = pathinfo( $filename );
-
-        if( class_exists( $namespace . $parts['filename'] ) ) {
-          $classname = $namespace . $parts['filename'];
-
-          $classname::Init();
-        }
+        $classname::Init();
       }
     }
   }
-}
-
-BlockLoader::Init();
+} );
